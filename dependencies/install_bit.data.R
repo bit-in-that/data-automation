fd3a96e420ce413b58f06a9457e348523fa8e15c
t-gummer <timothy.gummer@gmail.com>
2023-08-31 03:53:24 +1000

    fix dependencies again 3

--- dependencies/install_bit.data.R
+++ dependencies/install_bit.data.R
@@ -1,2 +1,2 @@
-# install.packages("checkmate", "httr", "jsonlite") # askpass
+install.packages(c("checkmate", "httr", "jsonlite")) # askpass
 install.packages("dependencies/bit.data_0.0.0.9001.tar.gz", repos = NULL)
