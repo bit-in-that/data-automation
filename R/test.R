8345579b054957bdfff5fce2672531e71a62aa2a
t-gummer <timothy.gummer@gmail.com>
2023-08-31 04:08:28 +1000

    fix dependencies again 4

--- R/test.R
+++ R/test.R
@@ -1,6 +1,2 @@
 cat(getwd())
 cat(here::here())
-
-x <- system("ipconfig", intern=TRUE)
-z <- x[grep("IPv4", x)]
-print(gsub(".*? ([[:digit:]])", "\\1", z))
--- dependencies/install_bit.data.R
+++ dependencies/install_bit.data.R
@@ -1,2 +1,2 @@
 install.packages(c("checkmate", "httr", "jsonlite")) # askpass
-install.packages("dependencies/bit.data_0.0.0.9001.tar.gz", repos = NULL)
+install.packages("dependencies/bit.data_0.0.0.9001.tar.gz", repos = NULL, dependencies = FALSE)
