7a5db0e56082e2e3af7d6726a73a76ad8156e5d3
t-gummer <timothy.gummer@gmail.com>
2023-08-31 12:01:31 +1000

    Test multiple workflows

--- R/fantasy_rankings.R
+++ R/fantasy_rankings.R
@@ -6,35 +6,49 @@ get_ranking_data <- function(team_ids, data_suffix) {
   session_id <- "e3526fbe4f1f94016a39a5a58cba41c5f744ea59"
   userids <- integer(length(team_ids))
   ranks <- integer(length(team_ids))
+  scores <- integer(length(team_ids))
   lineup_single <- list()
   # lineups_list <- list()
   ranking_single <- list()
   # rankings_list <- list()
   
-  for(team_id in team_ids) {
-    lineup_single <- get_af_classic_lineup_raw(session_id = session_id, team_id)
+  for(id in seq_along(team_ids)) {
+    lineup_single <- try(get_af_classic_lineup_raw(session_id = session_id, team_ids[id]), silent = TRUE)
     # lineups_list[[team_id]] <- lineup_single
-    ranking_single <- get_af_user_rank_raw(session_id = session_id, lineup_single$user_id)
-    # rankings_list[[team_id]] <- ranking_single
-    userids[team_id] <- lineup_single$user_id
-    ranks[team_id] <- ranking_single$rank
+    if(!is(lineup_single, "try-error")) {
+      ranking_single <- try(get_af_user_rank_raw(session_id = session_id, lineup_single$user_id), silent = TRUE)
+      # rankings_list[[team_id]] <- ranking_single
+      userids[id] <- lineup_single$user_id
+      scores[id] <- lineup_single$points
+      
+      if(!is(ranking_single, "try-error")) {
+        ranks[id] <- ranking_single$rank
+      } else {
+        ranks[id] <- NA_integer_
+      }
+      
+    } else {
+      userids[id] <- NA_integer_
+      ranks[id] <- NA_integer_
+      scores[id] <- NA_integer_
+    }
   }
   
   ranking_data <- data.frame(
     team_id = team_ids,
     userid = userids,
-    rank = ranks
+    rank = ranks,
+    score = scores
   )
-  
   
   # saveRDS(lineups_list, "outputs/fantasy_rankings/lineups_list.RDS")
   # saveRDS(rankings_list, "outputs/fantasy_rankings/rankings_list.RDS")
   
   suppressWarnings( dir.create("outputs/fantasy_rankings", recursive = TRUE))
   write_fst(ranking_data, paste0("outputs/fantasy_rankings/ranking_data", data_suffix, ".fst"))
-  
+  # ranking_data
 }
 
-# 163527L
-# get_ranking_data(team_ids = 1L:20L, "_0s")
+# 163550L
+# get_ranking_data(team_ids = 163548L:163555L, "_0s")
 
