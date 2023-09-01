b727f7dce2b2a0b17a2057c783e76b172ab5015a
t-gummer <timothy.gummer@gmail.com>
2023-09-01 21:44:26 +1000

    Update content creator data

--- fantasy_rankings/content_creators_ranks.R
+++ fantasy_rankings/content_creators_ranks.R
@@ -5,18 +5,17 @@ library(fst)
 
 content_creators <- data.table::fread("fantasy_rankings/data/raw/content_creators.csv") |> as_tibble()
 content_organisations <- data.table::fread("fantasy_rankings/data/raw/content_organisations.csv") |> as_tibble()
+overall_ranks_2022_top2550 <- read_fst("fantasy_rankings/data/processed/overall_ranks_2022_top2550.fst") |> select(user_id, rank_2022 = rank)
+overall_ranks_2023 <- read_fst("fantasy_rankings/data/processed/overall_ranks_2023.fst") |> select(user_id, rank_2023 = rank)
 
-
+  
 content_creators |> 
   filter(include) |>
   left_join(content_organisations, "organisation") |> 
-  mutate(
-    # TODO: join this on using the ranks dataset
-    rank_lists = map(user_id, get_af_user_rank_raw, session_id  = "e3526fbe4f1f94016a39a5a58cba41c5f744ea59"),
-    score = map_int(rank_lists, ~.x$points),
-    rank = map_int(rank_lists, ~.x$rank)
-  ) |> 
-  select(-rank_lists) -> content_creators_ranks
+  left_join(overall_ranks_2022_top2550, "user_id") |> 
+  left_join(overall_ranks_2023, "user_id") -> content_creators_ranks
+
+# "https://fantasy.afl.com.au/assets/media/avatars/afl/2673766.png?v=0"
 
 write_fst(content_creators_ranks, "fantasy_rankings/data/processed//content_creators_ranks.fst")
 
