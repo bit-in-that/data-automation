ac017b765250173a5c285542963381cef79bc437
t-gummer <timothy.gummer@gmail.com>
2024-03-19 14:41:20 +0800

    update content creator ranks

--- afl_fantasy/content_creator_ranks.R
+++ afl_fantasy/content_creator_ranks.R
@@ -14,6 +14,9 @@ overall_ranks_2022_top2550 <- read_parquet("afl_fantasy/data/processed/overall_r
 overall_ranks_2023 <- read_parquet("afl_fantasy/data/processed/overall_ranks_2023.parquet") |> 
   select(user_id, rank_2023 = rank)
 
+overall_ranks_2024 <- read_parquet("afl_fantasy/data/raw/2024/afl_fantasy_rankings.parquet") |> 
+  select(user_id, rank_2024 = rank)
+
 afl_fantasy_team_ids <- read_parquet("afl_fantasy/data/raw/2024/afl_fantasy_team_ids.parquet") |> 
   select(user_id, team_name, team_id)
 
@@ -26,6 +29,7 @@ content_creators_ranks <- content_creators |>
   left_join(content_organisations, "organisation") |> 
   left_join(overall_ranks_2022_top2550, "user_id") |> 
   left_join(overall_ranks_2023, "user_id") |> 
+  left_join(overall_ranks_2024, "user_id") |> 
   relocate(team_name, .after = name)
 
 write_parquet(content_creators_ranks, "afl_fantasy/data//processed/content_creators_ranks.parquet")
