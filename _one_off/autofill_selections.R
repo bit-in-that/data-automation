5c4cbfb7fe17a2474b373f50aa4d50ef21b46b96
t-gummer <timothy.gummer@gmail.com>
2024-03-20 16:34:43 +0800

    Update the selections processing for autofill

--- _one_off/autofill_selections.R
+++ _one_off/autofill_selections.R
@@ -3,23 +3,20 @@ library(dplyr)
 library(tidyr)
 library(arrow)
 
+autofill_date <- as.Date("2024-03-16")
+
 player_selections_long <- read_parquet("afl_fantasy/data/processed/2024/player_selections_long.parquet")
 af_player_data <- read_parquet("afl_fantasy/data/processed/2024/af_player_data.parquet") |> 
   select(id, team_name)
 
-
-# attach on a column indicating if player played before Saturday
-table(af_player_data$team_name)
-
-
-player_selections_long |> 
+autofill_picks <- player_selections_long |> 
   left_join(af_player_data, by = "id") |> 
   mutate(
     before_saturday = team_name %in% c("Richmond", "Carlton", "Sydney Swans", "Collingwood")
   ) |> 
   # player_selections_long |> 
   filter(
-    snapshot_date %in% as.Date(c("2024-03-15", "2024-03-16"))
+    snapshot_date %in% c(autofill_date - 1, autofill_date)
   ) |> 
   # select(id, first_name, last_name, before_saturday, team_name, selections, selections_diff, snapshot_date) |> 
   group_by(id, first_name, last_name, before_saturday, team_name) |> 
@@ -29,16 +26,20 @@ player_selections_long |>
     .groups = "drop"
   ) |>
   mutate(
-    increase_decrease_ratio = sum(change_previous * (change_previous > 0)) / sum(-change_previous * (change_previous < 0))
-  ) |> 
+    increase_decrease_ratio = sum(change_previous * (change_previous > 0)) / sum(-change_previous * (change_previous < 0)),
+    total_decreases_post_autofill = sum(-change_post_autofill * (change_post_autofill < 0)),
+    bau_increases_post_autofill = total_decreases_post_autofill * increase_decrease_ratio
+  ) |>
   # ASSUMPTION: we will only be making adjustments to players who have had an increase (assume anyone who decreased in ownership had negligible auto-fills)
   filter(change_post_autofill > 0) |> 
   mutate(
-    total_change_previous = sum(change_previous * !before_saturday * (change_previous > 0) * (change_post_autofill > 0)),
-    previous_change_post_saturday = if_else(before_saturday | (change_previous < 0) | (change_post_autofill < 0), 0, change_previous) / total_change_previous * 100
+    total_decreases_autofill = sum(-change_post_autofill * (change_post_autofill < 0)),
+    proportion_in_previous = change_previous / sum(change_previous),
+    bau_change_post_autofill = round(proportion_in_previous * bau_increases_post_autofill, 0),
+    selections_autofill = change_post_autofill - bau_change_post_autofill,
+    selections_autofill = if_else(selections_autofill > 0, selections_autofill, 0)
   ) |> 
-  View()
-  filter(selections > 0) |> 
-  pull(selections) |> 
-  sum()
+  select(id, selections_autofill)
 
+autofill_picks |> 
+  write_parquet("afl_fantasy/data/processed/2024/autofill_picks.parquet")
--- afl_fantasy/player_selection_processing.R
+++ afl_fantasy/player_selection_processing.R
@@ -12,9 +12,10 @@ bound_values <- function(x, max_value) {
   ) |> 
     round(digits = 2)
 }
-
+autofill_date <- as.Date("2024-03-16")
 
 player_selections_initial <- read_parquet("afl_fantasy/data/raw/2024/player_selections.parquet")
+autofill_picks <- read_parquet("afl_fantasy/data/processed/2024/autofill_picks.parquet")
 
 player_selections_long <- player_selections_initial |> 
   group_by(snapshot_time) |> 
@@ -29,10 +30,22 @@ player_selections_long <- player_selections_initial |>
   select(fantasy_coaches, snapshot_time) |> 
   distinct(fantasy_coaches, .keep_all = TRUE) |> 
   inner_join(player_selections_initial, y = _, by = "snapshot_time") |> 
-  group_by(snapshot_time, fantasy_coaches) |>
+  mutate(
+    snapshot_date = as.Date(snapshot_time, tz = "Australia/Sydney") - 1
+  ) |> 
+  left_join(autofill_picks, by = "id") |> 
+  mutate(
+    selections_autofill = coalesce(selections_autofill, 0L),
+    # overwrite the selections numbers with ones that are adjusted for autofills for all dates after the snapshot data
+    selections_no_autofill = if_else(snapshot_date >= autofill_date, selections - selections_autofill ,selections),
+    owned_by_no_autofill = round(selections_no_autofill / fantasy_coaches, 4)*100
+  ) |> 
+  group_by(snapshot_date, fantasy_coaches) |>
   mutate(
     completed_teams = sum(selections) / 30,
+    completed_teams_no_autofill = sum(selections_no_autofill) / 30,
     completion_percentage = completed_teams / fantasy_coaches,
+    completion_percentage_no_autofill = completed_teams_no_autofill / fantasy_coaches,
     completion_percentage_captain = sum(selections_captain) / 100,
     completion_percentage_vice_captain = sum(selections_vice_captain) / 100,
     completion_percentage_bench = sum(selections_bench) / 8 / 100,
@@ -40,8 +53,8 @@ player_selections_long <- player_selections_initial |>
   ) |> 
   ungroup() |> 
   mutate(
-    owned_by_adjusted = round(owned_by / completion_percentage, 2)
-  ) |> 
+    owned_by_adjusted = round(owned_by_no_autofill / completion_percentage_no_autofill, 2)
+  ) |>
   group_by(id) |> 
   mutate(
     selections_diff = c(head(selections, 1), diff(selections)),
@@ -52,7 +65,6 @@ player_selections_long <- player_selections_initial |>
   mutate(
     selections_adjusted_diff = round(owned_by_adjusted_diff * completed_teams / 100, 0),
     # snapshot_date = format(snapshot_time, format = "%Y_%m_%d"),
-    snapshot_date = as.Date(snapshot_time, tz = "Australia/Sydney") - 1,
     text_snapshot = paste0("Snapshot Date: ", format(snapshot_date, format = "%Y-%m-%d"), "<br />Owned By (Adjusted): ", round(owned_by_adjusted, 2), "%<br />Owned By (Official): ", round(owned_by, 2), "% (", selections,")<br /># Coaches: ", fantasy_coaches, " (", round(100*completion_percentage, 1), "% complete)"),
     text_daily_change = paste0("Snapshot Date: ", format(snapshot_date, format = "%Y-%m-%d"), "<br />Daily Movement In Adjusted Ownership: ", round(owned_by_adjusted_diff, 2), "% (", selections_adjusted_diff,")", "<br /># Coaches: ", fantasy_coaches, " (", round(100*completion_percentage, 1), "% complete)"),
     selections_captain_adjusted = bound_values(selections_captain / completion_percentage_captain, 100),
